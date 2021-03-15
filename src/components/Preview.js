69d3e474ba6e99c893e00c3096d0af7b3ad771f9
Ion Lazarev <vanealazarev@gmail.com>
2021-03-15 11:02:53 +0100

    Update Preview.js

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 class Preview extends Component {
   render() {
-    return <div></div>;
+    return;
+    <div className="container">
+      <div className="row">
+        <img src={this.props.photoUrl} />
+      </div>
+    </div>;
   }
 }
 
